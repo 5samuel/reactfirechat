7b3f7f548b8c8079a1189b2cd0dbf39e900f53f8
samuelangeles <samueljoseangelereye@gmail.com>
2021-03-30 13:49:59 -0600

    Add signIn and signOut functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 
 import Button from './components/Button';
-import{ googleAuthProvider, firebase} from './config/firebase';
-function App() {
+import {signInWithGoogle} from './login';
+import signOut from './login/signOut';
 
-  const signInWithGoogle = async ()=>{
-    firebase.auth().useDeviceLanguage();
-    try {
-      await firebase.auth().signInWithPopup(googleAuthProvider);
-    }catch (e){
-      console.error(e.message);
-    }
-  }
-  return (
-    
-       <center> 
+
+function App(){
+  return(
     <div>
-          <center><h3>React Firechat</h3></center>
-         <Button  onClick = {signInWithGoogle} > sing with Google</Button>
+      {
+        user ? (
+          <>
+          <Button onClick={signOut}> sign in with Google</Button>
+          <p>Bienvenidos al Chat!</p>
+          </>
+          ):<Button onClick={signInWithGoogle} > Sign in with Google</Button>
+        }
+        
     </div>
-    </center>
-
   );
 }
-
 export default App;
