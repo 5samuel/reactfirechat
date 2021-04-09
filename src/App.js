2fbc33e0d63cbb1e36480801cf012b1eacfe790d
samuelangeles <samueljoseangelereye@gmail.com>
2021-04-09 16:48:34 -0600

    Loading with initializing

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import {signInWithGoogle} from './login';
 import signOut from './login/signOut';
 import {useAuthState} from './hooks';
 import { firebase } from './config/firebase';
+import { render } from '@testing-library/react';
 
 
 function App(){
   const {user, initializing} = useAuthState(firebase.auth());
+
+  const renderLoading = ()=> {
+    if (initializing){
+      return <div>
+        <h1>Loading ...</h1>
+      </div>
+    }
+  }
   return(
     <div>
+      {renderLoading()}
       {
         user ? (
+          
           <>
           <Button onClick={signOut}> sign in with Google</Button>
           <p>Bienvenidos al Chat!</p>
