f4bccf8a7b1ff1b0d206845701678a5b5aabdefd
samuelangeles <samueljoseangelereye@gmail.com>
2021-04-10 23:49:15 -0600

    Add query for massages from firestore

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,7 +1,26 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const Channel = ({user = null}) =>{
-    const [message, setMessages]= useState([]);
+    const [messages, setMessages]= useState([]);
+    useEffect (() =>{
+        /*
+        crear el query de los mensaje.
+        carga 100 mensaje ordenados por fecha.       
+        */ 
+       const query = db.collection('messages')
+            .orderBy('createdAt')
+            .limit(100)
+        const unsubscribe = query.onSnapshot(querySnapshot =>{
+            //obtiene todos los mensajes desde la db con ID
+            const data = querySnapshot.docs.map(doc => ({
+                ...doc.data(),
+                id: doc.id,
+            }));
+            //Actualizando los mensajes obtenidos desde la base de datos.
+            setMessages(data);
+        });
+        return unsubscribe;
+    })
     return <ul></ul>
 };
 export default Channel;
