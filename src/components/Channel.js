681397b47bb7c013c5a6edf69c23ecaba8352057
samuelangeles <samueljoseangelereye@gmail.com>
2021-04-24 17:24:22 -0600

    reparacion del Channel

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,104 +1,108 @@
-import { useEffect, useState, useRef} from "react";
-import {db, firebase} from '../config/firebase';
+import { useEffect, useState, useRef } from 'react';
+import { db, firebase } from '../config/firebase';
 import Message from './Message';
 
-const Channel = ({user = null}) =>{
-    const [messages, setMessages]= useState([]);
-    //cargar los mensaje desde la base de datos.
-    useEffect (() =>{
-        /*
 
-        Crear el query de los mensajes.
-        Cargar 100 mensajes ordenados por fecha.
+const Channel = ({ user = null }) => {
+    const [messages, setMessages] = useState([]);
+
+    //Necesito cargar los mensajes desde la BD.
+    useEffect(() => {
+        /* 
+            Crear el query de los mensajes.
+            Cargar 100 mensajes ordenados por fecha.
         */
         const query = db.collection('messages')
-            .orderBy('creatdAt')
+            .orderBy('createdAt')
             .limit(100);
 
-        const unsubscribe = query.onSnapshot(querySnapshot =>{
-            //obtiene todos los mensajes desde la db con ID
+        const unsubscribe = query.onSnapshot(querySnapshot => {
+            //Obtiene todos los mensajes desde la bd con su ID.
             const data = querySnapshot.docs.map(doc => ({
                 ...doc.data(),
                 id: doc.id,
             }));
-            //Actualizando los mensajes obtenidos desde la base de datos.
+            // Actualizo los mensjaes obtenidos desde la bd. 
             setMessages(data);
 
         });
-
+        //CleanUp
         return unsubscribe;
 
-      }, []);
-    
-      const {uid, displayName, photoURL} = user;
-      const [newMessage, setNewMessage] = useState('');
-
-      const handleMessageOnChange = (e) => {
-          e.preventDefault();
-          setNewMessage(e.target.value);
-      }
-
-const messagesRef = db.collection('messages');
+    }, [])
 
 
-const handleOnSubmit = e => {
-    e.preventDefault();
+    //Codigo para agregar nuevos mensajes
+    const { uid, displayName, photoURL } = user;
+    const [newMessage, setNewMessage] = useState('');
 
-    const trimmedMessage = newMessage.trim();
-    if (trimmedMessage) {
-        //Add new message in firestore
-        messagesRef.add({
-text: trimmedMessage,
-createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-uid,
-displayName,
-photoURL,
-        });
-        //Clear input field
-
-        setNewMessage('');
+    const handleMessageOnChange = (e) => {
+        e.preventDefault();
+        setNewMessage(e.target.value);
     }
-}};
 
-const inputRef = useRef();
-useEffect(() => {
-    if (inputRef.current) {
-        inputRef.current.focus();
-    }
-}, [inputRef]);
 
-return (
-    <>
-    <ul>
-        {messages.map(message => (
-            <li key={message.id}>
-                <Message {...message} />
-                </li>
-        ))}
-    </ul>
+    const messagesRef = db.collection('messages');
 
-    <form
-    onSubmit={handleOnSubmit}>
 
-        <input
-        ref={inputRef}
-        type="text"
-        value={newMessage}
-        onChange={handleMessageOnChange}
-        placeholder="Escribe tu mensaje aqui..."
-        
-        />
+    const handleOnSubmit = e => {
+        e.preventDefault();
 
-        <button
-        type="submit"
-        disabled={!newMessage}
-        >
-            Send
-            </button>
+        const trimmedMessage = newMessage.trim();
+        if (trimmedMessage) {
+            // Add new message in Firestore
+            messagesRef.add({
+                text: trimmedMessage,
+                createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+                uid,
+                displayName,
+                photoURL,
+            });
+            // Clear input field
+            setNewMessage('');
+        }
+    };
+
+    const inputRef = useRef();
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [inputRef]);
+
+
+    return (
+        <>
+            <ul>
+                {messages.map(message => (
+                    <li key={message.id}>
+                        <Message {...message} />
+                    </li>
+                ))}
+            </ul>
+
+            <form
+                onSubmit={handleOnSubmit}>
+                <input
+                    ref={inputRef}
+                    type="text"
+                    value={newMessage}
+                    onChange={handleMessageOnChange}
+                    placeholder="Escribe tu mensaje aqui..."
+                />
+                <button
+                    type="submit"
+                    disabled={!newMessage}
+                >
+                    Send
+        </button>
             </form>
-            </>
-);
-        export default Channel;
+        </>
+    );
+};
+
+export default Channel;
+
     
     
 
